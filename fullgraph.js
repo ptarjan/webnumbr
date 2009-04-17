35a692c769bf4dae192bbc3f191750f9597c014e
Paul Tarjan <spam@paulisageek.com>
2009-04-17 12:29:34 -0700

    put all the data in one table

--- fullgraph.js
+++ fullgraph.js
@@ -26,71 +26,68 @@ paulisageek.wg.postGraphCallback = function(json) {
         $(":input[name='from']").val("");
     });
 
+    var query = $.param($.query.keys);
+    json.graphs.push({ "meta" : {
+        "API" : "ajax/v1/graph?" + query,
+        "embed" : ""
+    }});
+    var tbody = $(document.createElement("tbody"));
+    $("#data").append($(document.createElement("table")).append(tbody)).hide();
+
     for (var graphIndex in json.graphs) {
-       var graph = json.graphs[graphIndex];
+        var graph = json.graphs[graphIndex];
 
-        // If the node exists
-        if ($("#data").size() > 0) {
-            $("#data").hide();
-            var meta = graph.meta;
+        var meta = graph.meta;
 
-            var query = $.query.keys;
-            query.id = meta.id;
-            query = $.param(query);
-
-            var tbody = $(document.createElement("tbody"));
-            meta.API = "ajax/v1/graph?" + query;
-            meta.embed = "";
-            for (var key in meta) {
-                var tr = $(document.createElement("tr"));
-                var th = $(document.createElement("th"));
-                th.text(key).wrapInner("<b></b>");
-                tr.append(th);
-                var td = $(document.createElement("td"));
-                td.text(meta[key]);
-                switch (key) {
-                    case "url" : 
-                    case "API" :
-                        // Create an "a" around the element with the same content as the element
-                        td.wrapInner($(document.createElement("a")).attr("href", td.text()));
-                        break;
-                    case "embed" :
-                        var iframe = $(document.createElement("iframe"))
-                            .attr("frameborder", "0")
-                            .attr("src", "http://paulisageek.com/webGraphr/embedGraph?type=js&" + query)
-                            .css("width", "450px")
-                            .css("height", "300px")
-                            .css("display", "block");
-                        var input = $(document.createElement("input"))
-                            .click(function() {this.focus(); this.select(); })
-                            .attr("value", $(document.createElement("div")).append(iframe).html())
-                            .attr("size", 90);
-                        td.append(input);
-                        var a = $(document.createElement("a"))
-                            .attr("href",  "#")
-                            .click(function() {
-                                window.open('embedGraph?type=js&' + query, 'Embed Preview', 'width=450,height=300'); 
-                                return false;
-                            })
-                            .text("Preview");
-                        td.append(" ");
-                        td.append(a);
-                        break;
-                    case "xpath" :
-                        td.wrapInner($(document.createElement("a")).attr("href", 'selectNode?' + $.param({url: meta.url, xpath: meta.xpath})));
-                        break;
-                    case "createdTime" :
-                    case "modifiedTime" :
-                        td.text(new Date(meta[key] * 1000).toString());
-                        break;
-                }
-                tr.append(td);
-                tbody.append(tr);
+        for (var key in meta) {
+            var tr = $(document.createElement("tr"));
+            var th = $(document.createElement("th"));
+            th.text(key).wrapInner("<b></b>");
+            tr.append(th);
+            var td = $(document.createElement("td"));
+            td.text(meta[key]);
+            switch (key) {
+                case "url" : 
+                case "API" :
+                    // Create an "a" around the element with the same content as the element
+                    td.wrapInner($(document.createElement("a")).attr("href", td.text()));
+                    break;
+                case "embed" :
+                    var iframe = $(document.createElement("iframe"))
+                        .attr("frameborder", "0")
+                        .attr("src", "http://paulisageek.com/webGraphr/embedGraph?type=js&" + query)
+                        .css("width", "450px")
+                        .css("height", "300px")
+                        .css("display", "block");
+                    var input = $(document.createElement("input"))
+                        .click(function() {this.focus(); this.select(); })
+                        .attr("value", $(document.createElement("div")).append(iframe).html())
+                        .attr("size", 90);
+                    td.append(input);
+                    var a = $(document.createElement("a"))
+                        .attr("href",  "#")
+                        .click(function() {
+                            window.open('embedGraph?type=js&' + query, 'Embed Preview', 'width=450,height=300'); 
+                            return false;
+                        })
+                        .text("Preview");
+                    tr.append(td);
+                    var td = $(document.createElement("td"));
+                    td.append(a);
+                    break;
+                case "xpath" :
+                    td.wrapInner($(document.createElement("a")).attr("href", 'selectNode?' + $.param({url: meta.url, xpath: meta.xpath})));
+                    break;
+                case "createdTime" :
+                case "modifiedTime" :
+                    td.text(new Date(meta[key] * 1000).toString());
+                    break;
             }
-            // 0 for the first one, 30 for others
-            var margin = ($("#data").children().length > 0 ? 30 : 0);
-            $("#data").append($(document.createElement("table")).append(tbody).css("margin-top", margin));
+            tr.append(td);
+            tbody.append(tr);
         }
+
+        tbody.append("<tr><td>&nbsp;</td><td/></tr>");
     }
 
     $(':input').ready(function() {
