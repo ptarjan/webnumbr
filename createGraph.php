2723e25a271a0bf197d55592448b8c466e43ddae
Paul Tarjan <spam@paulisageek.com>
2009-04-18 20:01:12 -0700

    inlined the ga code

--- createGraph.php
+++ createGraph.php
@@ -152,8 +152,9 @@ $(document).ready(function() {
 
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+// document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
+<script src='http://google-analytics.com/ga.js' type='text/javascript'></script>
 <script type="text/javascript">
 try {
 var pageTracker = _gat._getTracker("UA-149816-4");
