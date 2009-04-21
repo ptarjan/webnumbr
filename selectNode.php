6e96e89969bb446874c165e697c4d97a633be743
Paul Tarjan <spam@paulisageek.com>
2009-04-20 22:54:57 -0700

    redid index page

--- selectNode.php
+++ selectNode.php
@@ -1,5 +1,55 @@
 <?php
-if (! isset($_REQUEST['url'])) die("Need URL parameter in query string");
+if (! isset($_REQUEST['url'])) {
+print '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>webGraphr - Graph anything on the web</title>
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset/reset-min.css" />
+    <link rel="stylesheet" href="/style.css" type='text/css' />  
+    <link rel="stylesheet" href="style.css" type='text/css' />  
+    
+    <link rel="icon" href="images/webGraphr-favicon.png" type="image/x-icon" />
+
+  </head>
+  <body>
+    <div id='container'>
+      <div id='header'>
+        <a href='.'><img id='logo' src="images/webGraphr-banner-100.png" alt="logo" /></a>
+      </div>
+
+      <div class="content">
+
+        <h1 id='start'>
+          Start a Graph
+        </h1>
+
+        <form action='selectNode'>
+          <div id="startForm"> 
+            <label id="urlLabel" for="url">URL:</label>
+            <input id="url" name='url' value="http://" />
+            <input id="submitURL" type='submit' value='Pick the Number on the Page' />
+          </div>
+        </form>
+      </div>
+    </div>
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.js'></script>
+<script type='text/javascript'>
+var resizeURL = function() {
+    $("#url").width($("#startForm").width() - $("#urlLabel").width() - $("#submitURL").width() - 30);
+};
+$("#startForm").ready(resizeURL);
+$(window).resize(resizeURL);
+</script>
+  </body>
+</html>
+
+<?php
+die();
+}
 if (! isset($_REQUEST['xpath'])) $_REQUEST['xpath'] = NULL;
 
 require ("fetch.inc");
