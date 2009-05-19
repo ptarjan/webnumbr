31e7f8c6f8d07a833fb4d76cf431bae7feb16f3a
Paul Tarjan <spam@paulisageek.com>
2009-05-19 02:40:06 -0700

    remote plugin

---
+++ numbrPlugins/operator/remote/code.php
@@ -0,0 +1,29 @@
+<?php
+// Hard parsing
+if (count($params) == 1 && isset($params[0])) {
+    $params['url'] = $params[0];
+}
+
+$url = $params['url'];
+// $url .= (strpos("?", $url) === FALSE ? '?' : '&');
+// $url .= http_build_query(array("c" => $c, "data" => $data, "params" => $params));
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+
+$json = json_decode($data, TRUE);
+$bad = FALSE;
+if ($json === FALSE) {
+    $bad = TRUE;
+} else if (!isset($json['data']) || !isset($json['c'])) { 
+    $bad = TRUE;
+}
+if ($bad) {
+    $data = "$url returned '$data'. Expected json {\"data\":3.14159,\"c\":{\"key\":\"value\"}}";
+} else {
+    $data = $json['data'];
+    foreach ($json['c'] as $k => $v)
+        $c[$k] = $v;
+}
+?>
