03b57ac6cb3977c5bc61992c849f9a9d078865d3
Paul Tarjan <spam@paulisageek.com>
2009-05-18 21:48:53 -0700

    use the full code

--- numbrPlugins/format/link/code.php
+++ numbrPlugins/format/link/code.php
@@ -1,1 +1,1 @@
-<a href="http://webnumbr.com/<?php print $c['name'] ?>"><?php print $data ?></a>
+<a href="http://webnumbr.com/<?php print str_replace(".link", "", $c['code']) ?>"><?php print json_encode($data) ?></a>
