6e1d5ed9202b56533321b48cba07d68809937f9a
Paul Tarjan <spam@paulisageek.com>
2009-05-19 20:13:34 -0700

    autocomplete

---
+++ numbrPlugins/selection/from/code.php
@@ -0,0 +1,9 @@
+<?php
+if (count($params) == 1 && isset($params[0]))
+    $params['date'] = $params[0];
+
+if (!is_numeric($params['date']) && strtotime($params['date']) !== FALSE)
+    $params['date'] = strtotime($params['date']);
+$c['sql']['where'][] = "UNIX_TIMESTAMP(timestamp) >= :fromdate";
+$c['sql']['params']['fromdate'] = ($params['date']);
+require("numbrPlugins/selection/all/code.php");
