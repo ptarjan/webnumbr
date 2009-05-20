516b4b037282188a2f0a4f60eb4aa01791638502
Paul Tarjan <spam@paulisageek.com>
2009-05-19 22:01:07 -0700

    defaults

--- numbrPlugins/selection/to/code.php
+++ numbrPlugins/selection/to/code.php
@@ -4,6 +4,9 @@ if (count($params) == 1 && isset($params[0]))
 
 if (!is_numeric($params['date']) && strtotime($params['date']) !== FALSE)
     $params['date'] = strtotime($params['date']);
+
+if (!isset($params['date'])) $params['date'] = pow(2,31)-1;
+
 $c['sql']['where'][] = "UNIX_TIMESTAMP(timestamp) <= :todate";
 $c['sql']['params']['todate'] = ($params['date']);
 require("numbrPlugins/selection/all/code.php");
