ebb1bf58a0b2fc30b1136578ee73d9d83ff05b63
Paul Tarjan <spam@paulisageek.com>
2009-05-28 18:21:05 -0700

    made default plugin work

--- numbrPlugins/format/default/code.php
+++ numbrPlugins/format/default/code.php
@@ -1,41 +1,59 @@
 <?php
+$stmt = $PDO->prepare("SELECT * FROM numbrs WHERE name = :name");
+$stmt->execute(array("name" => $c['name'])) || die(json_encode($stmt->errorInfo()));
+$sql = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (count($sql) == 0) die("No numbr found");
+$numbr = array();
+foreach ($sql[0] as $key => $val) 
+    $numbr[$key] = htmlspecialchars($val);
+$ch = curl_init("http://" . $_SERVER['HTTP_HOST'] . "/{$c['code']}.embed");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$embed = htmlentities(curl_exec($ch));
 
-
+$ch = curl_init("http://" . $_SERVER['HTTP_HOST'] . "/{$c['code']}.all.graph.embed");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$graphembed = htmlentities(curl_exec($ch));
 // ================ templates parts ===================
 
-$subtitle = "Numbr title";
+$subtitle = $numbr['title'];
 
 $content = <<<END
 
 <div class="numbr_card">
             <div class="numbr_title">
-                    Numbr title
+                    {$numbr['title']}
             </div>
             <div class="numbr_box">
                     $data              
             </div>
-<div class="numbr_embed_code">Embed code: <input type="text" value="embed code"/></div>
+<div class="numbr_embed_code">Embed code: <input type="text" value="$embed"/></div>
 
             <div class="clear"></div>
 
 <div class="numbr_graph">
-graph
+<iframe src="/{$c['code']}.all.graph" style="width: 100%; height: 400px;" allowtransparnecy="true" frameborder="0"></iframe>
 
-<br><div class="graph_embed_code">Embed code for graph: <input type="text" value="embed code"/></div>
+<br><div class="numbr_graph_embed_code">Embed code for graph: <input type="text" value="$graphembed"/></div>
 </div>
 
 
+            <h3>Description</h3> 
             <div class="numbr_description">
-                    <h3>Description</h3> 
-                    description text
-
-                 <br>&nbsp;<h3>Source</h3>
-
-                <a href="http://www.expedia.com/pub/agent.dll?qscr=fexp&amp;flag=q&amp;city1=SJC&amp;citd1=SXM&amp;date1=5/1/2009&amp;time1=362&amp;date2=5/9/2009&amp;time2=362&amp;cAdu=1&amp;cSen=&amp;cChi=&amp;cInf=&amp;infs=2&amp;tktt=&amp;trpt=2&amp;ecrc=&amp;eccn=&amp;qryt=8&amp;load=1&amp;airp1=&amp;dair1=&amp;rdct=1&amp;rfrr=-429">
-                    http://www.expedia.com/pub/agent.dll?qscr=fexp&amp;flag=q&amp;city1=SJC&amp;citd1=SXM&amp;date1=5/1/2009&amp;time1=36...                </a>
+                    {$numbr['description']}
             </div>
 
+             <h3>Source</h3>
 
+             <a href="{$numbr['url']}" class="numbr_url">
+                {$numbr['url']}
+             </a>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" ></script>
+<script>
+$("input").focus(function() {
+    $(this).select();
+});
+</script>
 
 END;
 
