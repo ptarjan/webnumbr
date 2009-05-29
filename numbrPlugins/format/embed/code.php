7721830753e7659d5ba7251b89c47fd0da58ee94
Paul Tarjan <spam@paulisageek.com>
2009-05-28 18:13:00 -0700

    embed strings too

--- numbrPlugins/format/embed/code.php
+++ numbrPlugins/format/embed/code.php
@@ -1,8 +1,13 @@
 <?php
-$id = "webNumbr_" . htmlentities($c['name']);
-$function = "webNumbr_" . htmlentities(str_replace("-", "_", $c['name']));
 $name = str_replace(".embed", "", $c['code']);
 
-print "<span id=\"$id\">" . json_encode($data) . "</span><script>var $function = function(data) { document.getElementById(\"$id\").innerHTML = data; }</script><script src=\"http://webnumbr.com/{$name}.json(callback=$function)\"></script>";
+if (is_string($data)) {
+    // It is probably already formatted so we should iframe it
+    print "<iframe src=\"http://webnumbr.com/{$name}\" style=\"width: 100%; height: 400px;\" allowtransparnecy=\"true\" frameborder=\"0\"></iframe>";
+} else {
+    $id = "webNumbr_" . htmlentities($c['name']);
+    $function = "webNumbr_" . htmlentities(str_replace("-", "_", $c['name']));
 
+    print "<span id=\"$id\">" . json_encode($data) . "</span><script>var $function = function(data) { document.getElementById(\"$id\").innerHTML = data; }</script><script src=\"http://webnumbr.com/{$name}.json(callback=$function)\"></script>";
+}
 ?>
