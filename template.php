5da3d71b5f297be173fcc76174bdad468e4613b3
Paul Tarjan <paul@paulisageek.com>
2010-01-27 01:35:59 -0800

    try keywords, see if they work

--- template.php
+++ template.php
@@ -22,8 +22,9 @@ $status = urlencode("@webnumbr http://webnumbr.com is $thought");
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
-        <title>WebNumbr | <?php print $subtitle ?></title>
+        <title>Web Numbr | <?php print $subtitle ?></title>
         <link rel="stylesheet" href="/style.css" type='text/css' />
+        <meta name="keywords" content="webnumbr, webnumber, web number, web numbr, web graph, web plot, web grapher" />
     </head>
     <body>
             <div id="wrap">
