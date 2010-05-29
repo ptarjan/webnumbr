e094f27a7b72f0f47d5fad2895854f6e46c53ce7
Paul Tarjan <paul@paulisageek.com>
2010-05-28 23:41:31 -0700

    name it after the numbr

--- numbrPlugins/format/gvis/code.php
+++ numbrPlugins/format/gvis/code.php
@@ -9,7 +9,7 @@ $vis = new MC_Google_Visualization($PDO);
 $vis->addEntity('numbr_data', array(
     'fields' => array(
         'timestamp' => array('field' => 'timestamp', 'type' => 'timestamp'),
-        'data' => array('field' => 'data', 'type' => 'number'),
+        $c['name'] => array('field' => 'data', 'type' => 'number'),
     ), 
     'where' => 'numbr = \''.$c['name'].'\''
 ));
