e7cfe852f58d283675f8a551730f4990154e120a
Paul Tarjan <paul@paulisageek.com>
2010-05-29 01:16:56 -0700

    support timestamp = false

--- numbrPlugins/format/gvis/code.php
+++ numbrPlugins/format/gvis/code.php
@@ -6,11 +6,14 @@ require_once 'MC/Google/Visualization.php';
 require '../../../db.inc';
 $vis = new MC_Google_Visualization($PDO);
 
+$fields = array();
+if (!$params['timestamp'])
+  $fields['timestamp'] = array('field' => 'timestamp', 'type' => 'timestamp');
+
+$fields[$c['name']] = array('field' => 'data', 'type' => 'number');
+
 $vis->addEntity('numbr_data', array(
-    'fields' => array(
-        'timestamp' => array('field' => 'timestamp', 'type' => 'timestamp'),
-        $c['name'] => array('field' => 'data', 'type' => 'number'),
-    ), 
+    'fields' => $fields,
     'where' => 'numbr = \''.$c['name'].'\''
 ));
 $vis->setDefaultEntity('numbr_data');
