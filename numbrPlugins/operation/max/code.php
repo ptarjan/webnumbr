7e2efef48b11c96d093732a0b06cde534b82501e
Paul Tarjan <paul@paulisageek.com>
2010-06-26 19:09:57 -0700

    ignore if not a number

--- numbrPlugins/operation/max/code.php
+++ numbrPlugins/operation/max/code.php
@@ -5,17 +5,18 @@ if (is_array($data)) {
     $num = (int) $params['num'];
   else if (isset($params[0]))
     $num = (int) $params[0];
-  if (!is_numeric($num))
-    $num = 1;
 
-  $newData = array();
-  foreach ($data as $row) {
-    $time = (float) $row[0];
-    $val = (float) $row[1];
-    if ($val <= $num) {
-      $newData[] = (array($time, $val));
+  // only do this is the num is numeric
+  if (is_numeric($num)) {
+    $newData = array();
+    foreach ($data as $row) {
+      $time = (float) $row[0];
+      $val = (float) $row[1];
+      if ($val <= $num) {
+        $newData[] = (array($time, $val));
+      }
     }
-  }
 
-  $data = $newData;
+    $data = $newData;
+  }
 }
