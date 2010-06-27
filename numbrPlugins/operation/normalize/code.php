b2a83f6ce18ab9346f6bd9f1e7c8dd5ba77123e3
Paul Tarjan <paul@paulisageek.com>
2010-06-26 18:54:27 -0700

    normalize

---
+++ numbrPlugins/operation/normalize/code.php
@@ -0,0 +1,26 @@
+<?php
+if (is_array($data)) {
+  $range = NULL;
+  if (isset($params['range']))
+    $range = (int) $params['range'];
+  else if (isset($params[0]))
+    $range = (int) $params[0];
+  if (!is_numeric($range))
+    $range = 1;
+
+  $max = 0;
+  foreach ($data as $row) {
+    $val = (float) $row[1];
+    $max = max($max, $val);
+  }
+
+  $newData = array();
+  foreach ($data as $row) {
+    $time = (float) $row[0];
+    $val = (float) $row[1];
+    $val /= $max;
+    $newData[] = (array($time, $val));
+  }
+
+  $data = $newData;
+}
